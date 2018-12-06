7a594578dd388f6d3c3c790196dcd5ef2bb0d267
JackWang <jack00770066@gmail.com>
2018-12-06 23:45:57 +0800

    write to excel

---
+++ products_write.py
@@ -0,0 +1,16 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	products.append([name, price])
+print(products)
+print(products[0][0])
+
+for p in products:
+	print(p[0], '的價格是', p[1])
+
+with open ('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
